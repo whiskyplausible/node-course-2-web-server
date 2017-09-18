8bb459c89af9d95bdf39621c8d97c9c30c260203
whiskyplausible <bumble@lagosta.org.uk>
2017-09-18 16:35:30 +0100

    Add tests for PATCH /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'First test todo'
 }, {
     _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }]
 
 beforeEach((done) => {
@@ -158,5 +160,42 @@ describe('DELETE /todos/:id', () => {
       .expect(404)
       .end(done);
   })
+
+})
+
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+          text: "this is now updated text",
+          completed: true })
+      .expect(200)
+      .expect((res) => {
+          expect(res.body.todo.text).toBe("this is now updated text");
+          expect(res.body.todo.completed).toBe(true);
+          //expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+
+  })
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+          completed: false })
+      .expect(200)
+      .expect((res) => {
+          expect(res.body.todo.completed).toBe(false);
+          expect(res.body.todo.completedAt).toBe(null);
+      })
+      .end(done);
+  })
+
+
 
 })
